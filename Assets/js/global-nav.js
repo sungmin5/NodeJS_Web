6f53e4cc0a674b95881f4631d715bea22d33a70e
sungmin5 <sungmin.sam5@gmail.com>
2020-03-27 21:11:53 -0700

    Adding more features to the global nav

---
+++ Assets/js/global-nav.js
@@ -0,0 +1,16 @@
+var previousScroll = 0;
+$(window).scroll(function () {
+
+	var currentScroll = $(this).scrollTop();
+  //console.log(currentScroll + " : " + previousScroll);
+	if (currentScroll < 100) {
+    console.log("TEST");
+	} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+		if (currentScroll > previousScroll) {
+			$('#div-global').addClass('is-hidden');
+		} else {
+			$('#div-global').removeClass('is-hidden');
+		}
+		previousScroll = currentScroll;
+	}
+});
